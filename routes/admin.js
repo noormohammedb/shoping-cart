fcaa3b8a05ab237ed841d461f8775b87bd8d207b
noormohammedb <nooru.mohad@gmail.com>
2020-09-26 00:05:38 +0530

    file upload error maintained code formated by pretter dotenv installed client page try to responsive with centerd

--- routes/admin.js
+++ routes/admin.js
@@ -1,67 +1,75 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
+router.get("/", function (req, res, next) {
   let products = [
     {
-      name: 'product 1',
-      category: 'category',
-      description: 'description',
+      name: "product 1",
+      category: "category",
+      description: "description",
       image: "https://prepbootstrap.com/Content/blog/bootstrap4.png",
-      price: 199
+      price: 199,
     },
     {
-      name: 'product 2',
-      category: 'category',
-      description: 'description',
-      image: "https://www.freecodecamp.org/news/content/images/size/w2000/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png",
-      price: 299
+      name: "product 2",
+      category: "category",
+      description: "description",
+      image:
+        "https://www.freecodecamp.org/news/content/images/size/w2000/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png",
+      price: 299,
     },
     {
-      name: 'product 3',
-      category: 'category',
-      description: 'description',
-      image: "https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/300/full/angular2.png",
-      price: 599
+      name: "product 3",
+      category: "category",
+      description: "description",
+      image:
+        "https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/300/full/angular2.png",
+      price: 599,
     },
     {
-      name: 'product 4',
-      category: 'category',
-      description: 'description',
-      image: "https://cdn-images-1.medium.com/max/1200/1*mMiAaYO35gwMnN7-mfRdNw.png",
-      price: 899
-    }
+      name: "product 4",
+      category: "category",
+      description: "description",
+      image:
+        "https://cdn-images-1.medium.com/max/1200/1*mMiAaYO35gwMnN7-mfRdNw.png",
+      price: 899,
+    },
   ];
   let hbsObject = {
     products,
     title: "admin ",
     admin: true,
-  }
-  res.render('admin/view-products', hbsObject)
+  };
+  res.render("admin/view-products", hbsObject);
 });
 
-router.get('/add-product', (req, res, next) => {
+router.get("/add-product", (req, res, next) => {
   let hbsObject = {
     title: "admin add-porducts",
-    admin: true
-  }
-  res.render('admin/add-product', hbsObject)
+    admin: true,
+  };
+  res.render("admin/add-product", hbsObject);
 });
 
-router.post('/add-product', (req, res, next) => {
+router.post("/add-product", (req, res, next) => {
   console.log(req.body);
-  save = req.files.image;
-  console.log(`file name : ${save.name} Size : ${save.size} md5 : ${save.md5}`);
-  locdir = `./uploaded/${save.name}`
-  save.mv(locdir,(error)=>{
-    if(error){
-      res.send('fileupload error')
-      throw error
-    } 
-    console.log('done');
-  })
-  res.render('admin/add-product', { title: "admin add-porducts", admin: true })
+  // console.log(req.files);
+  if (req.files) {
+    save = req.files.image;
+    console.log(`file name : ${save.name} Size : ${save.size} md5 : ${save.md5}`);
+    locdir = `${__dirname}/../public/uploaded/${save.name}`;
+    save.mv(locdir, (error) => {
+      if (error) {
+        res.send("fileupload error");
+        throw error;
+      }
+      console.log("done");
+    });
+  } else {
+    console.log("no file in this request");
+  }
+  res.render("admin/add-product", { title: "admin add-porducts", admin: true });
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,44 +1,47 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get("/", function (req, res, next) {
   let products = [
     {
-      name: 'product 1',
-      category: 'category',
-      description: 'description',
+      name: "product 1",
+      category: "category",
+      description: "description",
       image: "https://prepbootstrap.com/Content/blog/bootstrap4.png",
-      price: 199
+      price: 199,
     },
     {
-      name: 'product 2',
-      category: 'category',
-      description: 'description',
-      image: "https://www.freecodecamp.org/news/content/images/size/w2000/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png",
-      price: 299
+      name: "product 2",
+      category: "category",
+      description: "description",
+      image:
+        "https://www.freecodecamp.org/news/content/images/size/w2000/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png",
+      price: 299,
     },
     {
-      name: 'product 3',
-      category: 'category',
-      description: 'description',
-      image: "https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/300/full/angular2.png",
-      price: 599
+      name: "product 3",
+      category: "category",
+      description: "description",
+      image:
+        "https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/300/full/angular2.png",
+      price: 599,
     },
     {
-      name: 'product 4',
-      category: 'category',
-      description: 'description',
-      image: "https://cdn-images-1.medium.com/max/1200/1*mMiAaYO35gwMnN7-mfRdNw.png",
-      price: 899
-    }
+      name: "product 4",
+      category: "category",
+      description: "description",
+      image:
+        "https://cdn-images-1.medium.com/max/1200/1*mMiAaYO35gwMnN7-mfRdNw.png",
+      price: 899,
+    },
   ];
   let hbsObject = {
     products,
-    title: 'shopping cart',
-    admin: false
-  }
-  res.render('index', hbsObject);
+    title: "shopping cart",
+    admin: false,
+  };
+  res.render("index", hbsObject);
 });
 
 module.exports = router;
