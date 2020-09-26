15f363e7ecfccf069b42c1bd413a8f28a3b0c249
noormohammedb <nooru.mohad@gmail.com>
2020-09-27 00:36:53 +0530

    products serving from db to user dummy data removed front img fixed

--- routes/index.js
+++ routes/index.js
@@ -1,47 +1,19 @@
 var express = require("express");
 var router = express.Router();
+var dbOperation = require("../dbconfig/dbOperation")
+
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  let products = [
-    {
-      name: "product 1",
-      category: "category",
-      description: "description",
-      image: "https://prepbootstrap.com/Content/blog/bootstrap4.png",
-      price: 199,
-    },
-    {
-      name: "product 2",
-      category: "category",
-      description: "description",
-      image:
-        "https://www.freecodecamp.org/news/content/images/size/w2000/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png",
-      price: 299,
-    },
-    {
-      name: "product 3",
-      category: "category",
-      description: "description",
-      image:
-        "https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/300/full/angular2.png",
-      price: 599,
-    },
-    {
-      name: "product 4",
-      category: "category",
-      description: "description",
-      image:
-        "https://cdn-images-1.medium.com/max/1200/1*mMiAaYO35gwMnN7-mfRdNw.png",
-      price: 899,
-    },
-  ];
   let hbsObject = {
-    products,
     title: "shopping cart",
     admin: false,
   };
-  res.render("index", hbsObject);
+  dbOperation.getProduct().then((data) => {
+    console.log(data);
+    hbsObject.products = data
+    res.render("index", hbsObject);
+  })
 });
 
 module.exports = router;
